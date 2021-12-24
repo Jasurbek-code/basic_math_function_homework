fc3ab87bea391f4cb9479f7af8fa5d876013d112
Jasurbek-code <samyboyborada@gmail.com>
2021-12-24 22:11:52 +0500

    basic math function

--- basic_func11.py
+++ basic_func11.py
@@ -6,10 +6,10 @@
 # Input: a = -8
 # Output: 8
 
-def main(a):
-    return abs(a)
+def main(a):        # funksiya a argument bilan yaratildi
+    return abs(a)   # a argument faqat musbat qiymat qaytarish uchun abs() funksiyani ishlatamiz
 
-print(main(-8))
+print(main(-8))     # funksiyani chiqarish
 
 
 # git add basic_func11.py ; git commit -m "basic math function" ; git push
