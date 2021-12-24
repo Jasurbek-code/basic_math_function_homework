0b8293541dc2fff46c6f45b913ffa14c5eef3d6b
Jasurbek-code <samyboyborada@gmail.com>
2021-12-24 23:27:20 +0500

    basic math function

--- basic_func17.py
+++ basic_func17.py
@@ -10,6 +10,7 @@ import math              # math kutubxonasini o'zini chaqirib olamiz.
                          # bunda hohlagan funksiyani ishlatib olishimiz mumkin
 
 def main(a):
+    a = a
     return a             # a argumenga pi qiymatini o'zlashtiramiz
 
 print(main(math.pi))     # math kutubxonasidan pi ni ishlatamiz
